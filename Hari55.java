220ca3e6a05e48508892253c918e4acee2aa2815
NurlindaAhmad <147230095+NurlindaAhmad@users.noreply.github.com>
2023-12-02 22:47:40 +0800

    Hari55.java

---
+++ Hari55.java
@@ -0,0 +1,18 @@
+public class Main {
+	public static void main(String[] args) {
+		
+        int n = 10; 
+        System.out.println("Deret Fibonacci dengan " + n + " angka:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+
+    static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+}
